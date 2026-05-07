8c87e87a12d0a810f7ef2abf610d65d182b330ff
Ehasanul Haque <ehasanmd365@gmail.com>
2026-05-07 13:53:23 +0600

    backend updated

--- app.js
+++ app.js
@@ -21,12 +21,68 @@ app.get("/", (req, res) => {
   res.render("index",{title: "Chess Game"});
 });
 
+// io.on("connection", (uniqueSocket) => { //[User connects to server]
+//   console.log("A user connected: " + uniqueSocket);  
+//   uniqueSocket.on("joinGame", () => {  //“This specific user sent joinGame”.[Server listens from that user]
+//     console.log("Player joined: ");
+//     io.emit("playerJoined")  //server broadcasts to all clients that a player has joined [Server broadcasts]
+  
+// })
+
+// uniqueSocket.on("disconnect", () => {
+//     console.log("A user disconnected: " + uniqueSocket);
+// })
+
+
+// });
 io.on("connection", (uniqueSocket) => { //[User connects to server]
-  console.log("A user connected: " + uniqueSocket);  
-  uniqueSocket.on("joinGame", () => {  //“This specific user sent joinGame”.[Server listens from that user]
-    console.log("Player joined: ");
-    io.emit("playerJoined")  //server broadcasts to all clients that a player has joined [Server broadcasts]
-})
+  console.log("A user connected: " );  
+  
+  if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "w"); //socket.emit(eventName, data)
+  } else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "b");
+  } else {
+    uniqueSocket.emit("spectatorRole");
+  }
+
+  uniqueSocket.on("disconnect", () => { //socket.on(eventName, callback)
+    if(uniqueSocket.id === players.white) {
+        delete players.white;
+    } else if(uniqueSocket.id === players.black) {
+        delete players.black;
+    }
+  })
+  
+
+
+  uniqueSocket.on("move", (move) => {
+    
+    try {
+      if (chess.turn() === "w" && uniqueSocket.id !== players.white) 
+        return;
+      if (chess.turn() === "b" && uniqueSocket.id !== players.black) 
+        return;
+
+      const result = chess.move(move);
+
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move: ", move);
+        uniqueSocket.emit("invalidMove", move);
+      }
+    } catch (err) {
+      console.log(err);
+      uniqueSocket.emit("invalidMove", move);
+    }
+  })
+
+
 });
 
 server.listen(3000, () => {
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,6 +1,6 @@
-const socket = io(); //This line creates a connection to the server.
+const socket = io(); //This line send request automatically to server (io.on()) to establish connection. [Client connect to server]
 
-socket.emit("joinGame"); //client send event “I want to join the game”.[User send event to server]
+socket.emit("joinGame"); //client send an name "joinGame" event “I want to join the game”.[User send event to server]
 socket.on("playerJoined", () => { //“If server broadcast playerJoined, run this code” [Browser listens]
     console.log("A player has joined the game.");
 });
